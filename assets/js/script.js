17bef2e15d4b2887f4f1ffda020b0a252b01239d
vpratnicki <vanessa@pratnicki.net>
2022-08-24 19:15:33 -0400

    Added startQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,86 @@
+var startContainer = document.querySelector("start");
+var quizContainer = document.querySelector("quiz");
+
+var startButton = document.querySelector("start-button");
+
+// questions and anwsers
+var questions = [{
+    question: "Commonly used data types DO NO include.",
+    options: [{ text: "strings", isCorrect: false },
+    { text: "booleans", isCorrect: false },
+    { text: "alerts", isCorrect: true },
+    { text: "numbers", isCorrect: false }
+    ]
+},
+{
+    question: "The condition in an if/else statement is enclosed within ___.",
+    options: [{ text: "quotes", isCorrect: false },
+    { text: "curly brackets", isCorrect: false },
+    { text: "parentheses", isCorrect: true },
+    { text: "square brackets", isCorrect: false }
+    ]
+},
+{
+    question: "Arrays in JavaScript can be used to store ___.",
+    options: [{ text: "numbers and strings", isCorrect: false },
+    { text: "other arrays", isCorrect: false },
+    { text: "booleans", isCorrect: false },
+    { text: "all of the above", isCorrect: true }
+    ]
+},
+{
+    question: "String values must be enclosed within ___ when beging assigned to variables.",
+    option: [{ text: "commas", isCorrect: false },
+    { text: "curly brackets", isCorrect: false },
+    { text: "quotes", isCorrect: true },
+    { text: "parentheses", isCorrect: false }
+    ]
+},
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    a: [{ text: "JavaScript", isCorrect: false },
+    { text: "terminal/bash", isCorrect: false },
+    { text: "for loops", isCorrect: false },
+    { text: "console.log", isCorrect: true }]
+}
+]
+
+// Start quiz function
+function startQuiz(event) {
+    event.preventDefault();
+    startContainer.style.display = "none";
+    quizContainer.style.display = "block";
+}
+
+startButton.addEventListener("click", startQuiz);
+
+
+
+
 // create elements for question 1
-var h1El = document.createElement("h1");
-var paraEl = document.createElement("p");
-var q1A1El = document.createElement("button");
-var q1A2El = document.createElement("button");
-var q1A3El = document.createElement("button");
-var q1A4El = document.createElement("button");
+// var h1El = document.createElement("h1");
+// var paraEl = document.createElement("p");
+// var q1A1El = document.createElement("button");
+// var q1A2El = document.createElement("button");
+// var q1A3El = document.createElement("button");
+// var q1A4El = document.createElement("button");
 
 // provide the content for question 1
-h1El.textContent = "Question 1:";
-paraEl.textContent = "Commonly used data types DO NOT include:";
-q1A1El.textContent = "1. strings";
-q1A2El.textContent = "2. booleans";
-q1A3El.textContent = "3. alerts";
-q1A4El.textContent = "4. numbers";
+// h1El.textContent = "Question 1:";
+// paraEl.textContent = "Commonly used data types DO NOT include:";
+// q1A1El.textContent = "1. strings";
+// q1A2El.textContent = "2. booleans";
+// q1A3El.textContent = "3. alerts";
+// q1A4El.textContent = "4. numbers";
 
 
 // append to question container on page (question 1)
-document.getElementById("question-container").appendChild(h1El);
-document.getElementById("question-container").appendChild(paraEl);
-document.getElementById("question-container").appendChild(q1A1El);
-document.getElementById("question-container").appendChild(q1A2El);
-document.getElementById("question-container").appendChild(q1A3El);
-document.getElementById("question-container").appendChild(q1A4El);
-
-
-
-
-// question anwsers
-var questions = [{
-    id: 1,
-    q: "Commonly used data types DO NO include.",
-    a: [{ text: "strings", isCorrect: false },
-        { text: "booleans", isCorrect: false },
-        { text: "alerts", isCorrect: true },
-        { text: "numbers", isCorrect: false }
-        ]
-    },
-    {
-        id: 2,
-        q: "The condition in an if/else statement is enclosed within ___.",
-        a: [{text: "quotes", isCorrect: false},
-            {text: "curly brackets", isCorrect: false},
-            {text: "parentheses", isCorrect: true},
-            {text: "square brackets", isCorrect: false}
-        ]
-    },
-    {
-        id: 3,
-        q: "Arrays in JavaScript can be used to store ___.",
-        a: [{text: "numbers and strings", isCorrect: false},
-            {text: "other arrays", isCorrect: false},
-            {text: "booleans", isCorrect: false},
-            {text: "all of the above", isCorrect: true}
-        ]
-    },
-    {
-        id: 4,
-        q: "String values must be enclosed within ___ when beging assigned to variables.",
-        a: [{text: "commas", isCorrect: false},
-            {text: "curly brackets", isCorrect: false},
-            {text: "quotes", isCorrect: true},
-            {text: "parentheses", isCorrect: false}
-        ]
-    },
-    {
-        id: 5,
-        q: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        a: [{text: "JavaScript", isCorrect: false},
-            {text: "terminal/bash", isCorrect: false},
-            {text: "for loops", isCorrect: false},
-            {text: "console.log", isCorrect: true}]
-    }
-]
-
-
+// document.getElementById("question-container").appendChild(h1El);
+// document.getElementById("question-container").appendChild(paraEl);
+// document.getElementById("question-container").appendChild(q1A1El);
+// document.getElementById("question-container").appendChild(q1A2El);
+// document.getElementById("question-container").appendChild(q1A3El);
+// document.getElementById("question-container").appendChild(q1A4El);
 
 
 // for each option 
@@ -88,10 +95,10 @@ var questions = [{
 
 // function evaluateAnswer(event) {
 //     if {
-        
+
 //     }
 
-    console.log(event);
+// console.log(event);
     // find out what question is on the screen
 
     // find out what answer is for that question
