264218691d9f49fd961ba8d0add874a8f2174b2d
vpratnicki <vanessa@pratnicki.net>
2022-08-25 22:08:18 -0400

    Created askQuestion and answerQuestion functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,123 +3,141 @@ var quizContainer = document.querySelector(".quiz");
 
 var startButton = document.querySelector(".start-button");
 
+var qIndex = 0;
 
 // questions and anwsers
-var questions = [{
-    id: 0,
+var questions = [
+  {
     question: "Commonly used data types DO NO include.",
-    options: [{ text: "strings", isCorrect: false },
-    { text: "booleans", isCorrect: false },
-    { text: "alerts", isCorrect: true },
-    { text: "numbers", isCorrect: false }
-    ]
-}
-// {
-//     question: "The condition in an if/else statement is enclosed within ___.",
-//     options: [{ text: "quotes", isCorrect: false },
-//     { text: "curly brackets", isCorrect: false },
-//     { text: "parentheses", isCorrect: true },
-//     { text: "square brackets", isCorrect: false }
-//     ]
-// },
-// {
-//     question: "Arrays in JavaScript can be used to store ___.",
-//     options: [{ text: "numbers and strings", isCorrect: false },
-//     { text: "other arrays", isCorrect: false },
-//     { text: "booleans", isCorrect: false },
-//     { text: "all of the above", isCorrect: true }
-//     ]
-// },
-// {
-//     question: "String values must be enclosed within ___ when beging assigned to variables.",
-//     options: [{ text: "commas", isCorrect: false },
-//     { text: "curly brackets", isCorrect: false },
-//     { text: "quotes", isCorrect: true },
-//     { text: "parentheses", isCorrect: false }
-//     ]
-// },
-// {
-//     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-//     options: [{ text: "JavaScript", isCorrect: false },
-//     { text: "terminal/bash", isCorrect: false },
-//     { text: "for loops", isCorrect: false },
-//     { text: "console.log", isCorrect: true }]
-// }
-]
+    options: [
+      { text: "strings", isCorrect: false },
+      { text: "booleans", isCorrect: false },
+      { text: "alerts", isCorrect: true },
+      { text: "numbers", isCorrect: false },
+    ],
+  },
+  {
+    question: "The condition in an if/else statement is enclosed within ___.",
+    options: [
+      { text: "quotes", isCorrect: false },
+      { text: "curly brackets", isCorrect: false },
+      { text: "parentheses", isCorrect: true },
+      { text: "square brackets", isCorrect: false },
+    ],
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ___.",
+    options: [
+      { text: "numbers and strings", isCorrect: false },
+      { text: "other arrays", isCorrect: false },
+      { text: "booleans", isCorrect: false },
+      { text: "all of the above", isCorrect: true },
+    ],
+  },
+  {
+    question:
+      "String values must be enclosed within ___ when beging assigned to variables.",
+    options: [
+      { text: "commas", isCorrect: false },
+      { text: "curly brackets", isCorrect: false },
+      { text: "quotes", isCorrect: true },
+      { text: "parentheses", isCorrect: false },
+    ],
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: [
+      { text: "JavaScript", isCorrect: false },
+      { text: "terminal/bash", isCorrect: false },
+      { text: "for loops", isCorrect: false },
+      { text: "console.log", isCorrect: true },
+    ],
+  },
+];
 
 // Start quiz function
 function startQuiz(event) {
-    event.preventDefault();
-    startContainer.style.display = "none";
-    quizContainer.style.display = "block";
+  event.preventDefault();
+  startContainer.style.display = "none";
+  quizContainer.style.display = "block";
+  askQuestion();
+}
+
+function askQuestion() {
+  var currentQuestion = questions[qIndex];
+  console.log(currentQuestion.question)
+  answerQuestion()
+}
+
+function answerQuestion() {
+  //do answer question things here
+  qIndex++;
+  if (qIndex < questions.length) {
+    askQuestion();
+  }
+  else {
+    console.log('finisehd at inex ' + qIndex)
+  }
 }
 
 startButton.addEventListener("click", startQuiz);
 
-
 //Get question elements to appear on page
 
-// Iterate
+// let list = document.getElementById("#options");
+
+// questions.question.forEach((item) => {
+//     let li = document.createElement("li");
+//     li.innerText = item;
+//     list.appendChild(li);
+// });
+
+// display and loop through
 // function iterate(id) {
-  
-    // Getting the result display section
-    // var result = document.getElementsByClassName(".result");
-    // result[1].innerText = "";
+
+// Getting the result display section
+// var result = document.getElementsByClassName(".result");
+// result[1].innerText = "";
 
 // Getting the question
-    // var quizContainer = document.getElementById("#quiz");
-  
-  
-    // Setting the question text
-    quizContainer.innerText = questions.question;
-  
-    // Getting the options
-    var op1 = document.getElementById('#op1');
-    var op2 = document.getElementById('#op2');
-    var op3 = document.getElementById('#op3');
-    var op4 = document.getElementById('#op4');
-  
-  
-    // Providing option text 
-    op1.innerText = questions.options[0].text;
-    op2.innerText = questions.options[1].text;
-    op3.innerText = questions.options[2].text;
-    op4.innerText = questions.options[3].text;
-  
-    // Providing the true or false value to the options
-    op1.value = questions.options[0].isCorrect;
-    op2.value = questions.options[1].isCorrect;
-    op3.value = questions.options[2].isCorrect;
-    op4.value = questions.options[3].isCorrect;
-  
+// var quizContainer = document.getElementById("#quiz");
 
+// Setting the question text
+// quizContainer.innerText = questions.question;
 
+// // Getting the options
+// var op1 = document.getElementById('#op1');
+// var op2 = document.getElementById('#op2');
+// var op3 = document.getElementById('#op3');
+// var op4 = document.getElementById('#op4');
 
+// // Providing option text
+// op1.innerText = questions.options[0].text;
+// op2.innerText = questions.options[1].text;
+// op3.innerText = questions.options[2].text;
+// op4.innerText = questions.options[3].text;
 
-
-
-
-
+// // Providing the true or false value to the options
+// op1.value = questions.options[0].isCorrect;
+// op2.value = questions.options[1].isCorrect;
+// op3.value = questions.options[2].isCorrect;
+// op4.value = questions.options[3].isCorrect;
 
 // var ol = document.createElement("ol");
 
-// for (let i of questions.options) { 
-//     let li = document.createElement("li"); 
-//     li.innerHTML = i; 
-//     ol.appendChild(li); 
+// for (let i of questions.options) {
+//     let li = document.createElement("li");
+//     li.innerHTML = i;
+//     ol.appendChild(li);
 // }
 // document.getElementById("quiz").appendChild(ol);
-
-
 
 // var questionEl = document.createElement("h3");
 // var optionsEl = document.createElement("ul");
 // var listItem = document.createElement('li'),
 
-
 // optionsEl.appendChild(listItem);
-
-
 
 // indexOf()
 
@@ -139,7 +157,6 @@ startButton.addEventListener("click", startQuiz);
 // q1A3El.textContent = "3. alerts";
 // q1A4El.textContent = "4. numbers";
 
-
 // append to question container on page (question 1)
 // document.getElementById("question-container").appendChild(h1El);
 // document.getElementById("question-container").appendChild(paraEl);
@@ -148,8 +165,7 @@ startButton.addEventListener("click", startQuiz);
 // document.getElementById("question-container").appendChild(q1A3El);
 // document.getElementById("question-container").appendChild(q1A4El);
 
-
-// for each option 
+// for each option
 // create some sort of button el that can hold an option
 // give it any attr that are most godderist /*value of choice*/
 // add an event listener that eval if the choice was correct
@@ -165,20 +181,19 @@ startButton.addEventListener("click", startQuiz);
 //     }
 
 // console.log(event);
-    // find out what question is on the screen
+// find out what question is on the screen
 
-    // find out what answer is for that question
+// find out what answer is for that question
 
-    // find out what button was clicked
+// find out what button was clicked
 
-    // determine if they were correct
-    // penalty or points ( come back to this)
+// determine if they were correct
+// penalty or points ( come back to this)
 
-    // remove the current container
-    // add the next container
-    // if thats the last question do the hi score thing
+// remove the current container
+// add the next container
+// if thats the last question do the hi score thing
 // }
-
 
 // let value = { tutor: "byron"};
 // value = JSON.stringify(value);
@@ -186,5 +201,3 @@ startButton.addEventListener("click", startQuiz);
 
 // let item = localStorage.getItem("key");
 // item = JSON.parse(item);
-
-
